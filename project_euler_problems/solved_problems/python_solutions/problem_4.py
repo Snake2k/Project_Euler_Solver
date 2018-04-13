9675147c81e7c8a2f949e1067f78c99efca169ca
Nobody <snake2k@localhost.localdomain>
2018-04-12 23:27:10 -0400

    Added problem 4 & dynamic importing of solutions.

---
+++ project_euler_problems/solved_problems/python_solutions/problem_4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+def problem_4():
+    answer = 0
+    for x in range(99, 1000):
+        for y in range(99, 1000):
+            product = x * y
+            if str(product) == str(product)[::-1] and \
+               product > answer:
+                answer = product
+    return answer
+
+if __name__=="__main__":
+    print("Problem 4 Answer: " + str(problem_4()))
