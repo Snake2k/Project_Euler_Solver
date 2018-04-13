b9e37f5eefcc97ef52a63fb7a5eda2e8a5c7ab61
Nobody <snake2k@localhost.localdomain>
2018-04-12 23:35:30 -0400

    Minor changes to how all solutions are added to the list.

--- project_euler_problems/problem_handler.py
+++ project_euler_problems/problem_handler.py
@@ -30,32 +30,25 @@ class Problem():
 # Create a list that contains the object for each solved problem.
 all_problems = []
 
-# Create the Problem object for each solved problem
-# Then add is to the list of all_problems (solved).
+# Create the Problem object for each solved problem.
 P1 = Problem(
         1,
         "Multiples of 3 and 5",
         "Sum up all the multiples of 3 and 5 below 1000",
         get_solution(1)
 )
-all_problems.append(P1)
-
 P2 = Problem(
         2,
         "Even Fibonacci numbers",
         "Sum of even-valued Fibonacci numbers under 4M.",
         get_solution(2)
 )
-all_problems.append(P2)
-
 P3 = Problem(
         3,
         "Largest Prime Factor",
         "Find the largest prime factor of 600851475143.",
         get_solution(3)
 )
-all_problems.append(P3)
-
 P4 = Problem(
         4,
         "Largest Palindrome Product",
@@ -63,4 +56,5 @@ P4 = Problem(
         "product of two 3-digit numbers.",
         get_solution(4)
 )
-all_problems.append(P4)
+# Add all Problem instances into the list of all_problems (solved).
+all_problems.extend([P1, P2, P3, P4])
